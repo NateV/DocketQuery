bd8fc46b8cccabd44775d57de2057e4323ce6197
NateV <nathan.c.vogel@gmail.com>
2015-04-21 21:12:35 -0400

    seems to be workin pretty much

--- docket_query.py
+++ docket_query.py
@@ -2,6 +2,9 @@ from lxml import etree
 import datetime
 from fractions import Fraction
 import csv
+import os
+import glob
+import re
 
 import pytest
 
@@ -20,6 +23,22 @@ Outline:
 TODO: A user can't customize queries without editing this module. Maybe
       fix that?
 """
+
+def query_directory(path, destination):
+  """
+  In: A path to a directory containing xml files representing dockets
+  Inside:
+  Output: A list of dicts written.
+  """
+  records = []
+  files_iterator = glob.iglob(path) #should make the glob more restrictive.
+  for file in files_iterator:
+    docket = Docket(file)
+    new_records = docket.get_guilty_sequence_records()
+    records = records + new_records
+
+  write_guilty_sequence_records(records, destination, mode="a")
+  return records
 
 def load_from_path(path):
   """
@@ -45,15 +64,49 @@ def convert_time(period, unit):
       ("7 1/2", "years")
   Out: A timedelta object of the input time period.
   """
-  if unit=="years":
+  year_pattern = re.compile("year", flags=re.I)
+  month_pattern = re.compile("month", flags=re.I)
+  if re.search(year_pattern, unit) != None:
     day_count = sum([float(Fraction(quantity)) * 365 for quantity in period.split()])
-  elif unit=="months":
-    day_count = sum([float(Fraction(quantty)) * 30.42]) # 30.41666 is the average
-                                                   # length of a month in non
-                                                   # leap-year.
+  elif re.search(month_pattern, unit) != None:
+    day_count = sum([float(Fraction(quantity)) * 30.42 for quantity in period.split()])
+                # 30.41666 is the average length of a month in non
+                # leap-year.
   else:
     day_count = 0
   return datetime.timedelta(days=day_count)
+
+def scrape_sentence_info(sentence):
+  """
+  Input: an etree.Element looking like:
+  <sentence_info>
+              <program>IPP</program>
+              <length_of_sentence>
+                <min_length>
+                  <time>12.00</time>
+                  <unit>Months</unit>
+                </min_length>
+                <max_length>
+                  <time>12.00</time>
+                  <unit>Months</unit>
+                </max_length>
+              </length_of_sentence>
+              <date>03/02/2011</date>
+              <extra_sentence_details>12 months... HOUSE ARREST FOR FIRST 3 MONTHS. SHERIFF TO T RANSPORT... Complete 20 hours community service...</extra_sentence_details>
+            </sentence_info>
+  Output: A dict looking like:
+        {"sentence_program": "IPP",
+         "min_length": <timedelta days=365>,
+         "max_length": <timedelta days=365>}
+  """
+  program = sentence.xpath("program/text()")[0].strip()
+  min_length_time = sentence.xpath("length_of_sentence/min_length/time/text()")[0].strip()
+  min_length_unit = sentence.xpath("length_of_sentence/min_length/unit/text()")[0].strip()
+  max_length_time = sentence.xpath("length_of_sentence/max_length/time/text()")[0].strip()
+  max_length_unit = sentence.xpath("length_of_sentence/max_length/time/text()")[0].strip()
+  return {"sentence_program": program,
+          "min_length": convert_time(min_length_time, min_length_unit),
+          "max_length": convert_time(max_length_time, max_length_unit)}
 
 def scrape_action(action):
   """
@@ -84,53 +137,46 @@ def scrape_action(action):
            </extra_sentence_details>
            </sentence_info>
          </judge_action>
-  Output: A dict looking like:
+  Output: A list containing dicts looking like:
     {
      "judge": "Hill, Glynnis",
-     "date": "09/09/2011",
+     "action_date": "09/09/2011",
      "sentence_program": "Confinement",
      "min_length": <timedelta object>,
      "max_length": <timedelta object>
     }
   """
+  actions = []
   judge = action.xpath("judge_name/text()")[0].strip()
   date = action.xpath("date/text()")[0].strip()
-  program = action.xpath("sentence_info/program/text()")[0].strip()
+  base_action = {"judge":judge,
+                 "action_date":date}
+  for sentence in action.xpath("sentence_info"):
+    new_action = scrape_sentence_info(sentence)
+    new_action.update(base_action)
+    actions.append(new_action)
+  return actions
 
-  min_length_period = action.xpath("sentence_info/length_of_sentence/min_length/time/text()")[0].strip()
-  min_length_unit = action.xpath("sentence_info/length_of_sentence/min_length/unit/text()")[0].strip()
-  min_length = convert_time(min_length_period, min_length_unit)
-
-  max_length_period = action.xpath("sentence_info/length_of_sentence/max_length/time/text()")[0].strip()
-  max_length_unit = action.xpath("sentence_info/length_of_sentence/max_length/unit/text()")[0].strip()
-  max_length = convert_time(max_length_period, max_length_unit)
-
-  return     {"judge": judge,
-              "date": date,
-              "sentence_program": program,
-              "min_length": min_length,
-              "max_length": max_length}
-
-def write_guilty_sequence_records(docket, destination, mode="w+"):
+def write_guilty_sequence_records(records, destination, mode="w+"):
   """
-  Input: 1) A Docket object,
+  Input: 1) Records from dockets,
          2) the name of a .csv file for saving the output
          3) File writing mode. Default is to create a new file/overwrite
             existing file.  "ab+" will probably be the other mode to use, for
             when processing many dockets and writing a .csv with lots of
             observations.
-  Output: "Success" on success, and the method saves a .csv file
+  Output: The list of dicts with the observations recorded.
   """
-  with open(destination, mode) as csvfile:
-    fieldnames =  ["docket_number", "defendant_name", "birth_date", "judge", "date",
+  with open(destination, mode, newline='') as csvfile:
+    fieldnames =  ["docket_number", "defendant_name", "birth_date", "judge", "action_date",
                    "charge", "disposition", "sentence_program", "min_length",
                    "max_length"]
     writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
     writer.writeheader()
-    for record in docket.get_guilty_sequence_records():
+    for record in records:
       writer.writerow(record)
   csvfile.close()
-  return "Success"
+  return records
 
 def get_actions_with_sentences(sequence):
   """
@@ -192,8 +238,27 @@ class Docket():
        disposition = sequence.xpath("offense_disposition/text()")[0].strip()
        actions_with_sentences = get_actions_with_sentences(sequence)
        for action in actions_with_sentences:
-         action_dict = scrape_action(action)
-         action_dict.update(base_record)
-         action_dict.update({"charge":charge, "disposition":disposition})
-         records.append(action_dict)
+         action_list = scrape_action(action)
+         for action_dict in action_list:
+           action_dict.update(base_record)
+           action_dict.update({"charge":charge, "disposition":disposition})
+           records.append(action_dict)
     return records
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test_docket_query.py
+++ test_docket_query.py
@@ -1,11 +1,13 @@
 from docket_query import Docket, load_from_path, load_tree_from_path, \
                          convert_time, scrape_action, \
                          write_guilty_sequence_records, \
-                         get_actions_with_sentences
+                         get_actions_with_sentences, query_directory, \
+                         scrape_sentence_info
 import datetime
 from io import StringIO
 from lxml import etree
 import pytest
+import os
 
 def test_load_from_path():
   path = "./test/texts/CP-51-CR-0000001-2011_stitched_complete.xml"
@@ -48,13 +50,33 @@ def test_scrape_action():
                        </sentence_info>
                        </judge_action>"""))
   action = scrape_action(action)
-  assert action["date"] == "09/09/2011"
-  assert action["min_length"] == datetime.timedelta(days=2737.5)
-  assert action["judge"] == "Hill, Glynnis"
+  assert action[0]["action_date"] == "09/09/2011"
+  assert action[0]["min_length"] == datetime.timedelta(days=2737.5)
+  assert action[0]["judge"] == "Hill, Glynnis"
+
+def test_scrape_sentence_info():
+  sentence_info = etree.parse(StringIO("""<sentence_info>
+              <program>IPP</program>
+              <length_of_sentence>
+                <min_length>
+                  <time>12.00</time>
+                  <unit>Months</unit>
+                </min_length>
+                <max_length>
+                  <time>12.00</time>
+                  <unit>Months</unit>
+                </max_length>
+              </length_of_sentence>
+              <date>03/02/2011</date>
+              <extra_sentence_details>12 months... HOUSE ARREST FOR FIRST 3 MONTHS. SHERIFF TO T RANSPORT... Complete 20 hours community service...</extra_sentence_details>
+            </sentence_info> """))
+  sentence_info = scrape_sentence_info(sentence_info)
+  assert sentence_info["min_length"] == convert_time("12","months")
 
 def test_write_guilty_sequence_records():
   docket = Docket("./test/texts/CP-51-CR-0000001-2011_stitched_complete.xml")
-  assert write_guilty_sequence_records(docket, "./test/output/records.csv") == "Success"
+  records = docket.get_guilty_sequence_records()
+  assert len(write_guilty_sequence_records(records, "./test/output/single_test_records.csv")) == 3
 
 def test_get_actions_with_sentences():
   sequence = etree.parse(StringIO("""<sequence>
@@ -91,6 +113,13 @@ def test_get_actions_with_sentences():
   assert len(actions) == 1
   assert actions[0].xpath("date/text()")[0].strip() == "09/09/2011"
 
+def test_query_directory():
+  directory_path = "./test/text1/*"
+  destination = "./test/output/query_directory_records.csv"
+  if os.path.exists(destination):
+    os.remove("./test/output/query_directory_records.csv")
+  records_written = query_directory(directory_path, destination)
+  assert len(records_written) == 10
 
 class TestDocket:
     #TODO - need to test what get returns if docket is broken.
@@ -117,7 +146,7 @@ class TestDocket:
     docket = Docket("./test/texts/CP-51-CR-0000001-2011_stitched_complete.xml")
     records = docket.get_guilty_sequence_records()
     assert records[0]["judge"] == "Hill, Glynnis"
-    assert records[0]["date"] == "09/09/2011"
+    assert records[0]["action_date"] == "09/09/2011"
     assert records[0]["charge"] == "Rape Forcible Compulsion"
 
 
